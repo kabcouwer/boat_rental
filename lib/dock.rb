de922c9edaa6e1a758f0a0ca2bd75528fed50d9d
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-03 07:40:28 -0600

    Add iteration 3

--- lib/dock.rb
+++ lib/dock.rb
@@ -12,4 +12,16 @@ class Dock
     rental_log[boat] = renter
   end
 
+  def charge(boat)
+    charge_hash = {}
+    charge_hash[:card_number] = rental_log[boat].credit_card_number
+    if boat.hours_rented <= @max_rental_time
+      charge_hash[:amount] = boat.price_per_hour * boat.hours_rented
+      charge_hash
+    else
+      charge_hash[:amount] = boat.price_per_hour * @max_rental_time
+      charge_hash
+    end
+  end
+
 end
